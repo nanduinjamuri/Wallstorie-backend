f2da8a1ecfe9dad8ef4d33e41602dfa39944585c
nanduinjamuri <nanduinjamuri1234@gmail.com>
2025-06-07 17:17:00 +0530

    Update passportConfig.js

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -11,7 +11,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        "https://wallstorie-backend.onrender.com/api/auth/google/callback",
+        "https://wallstorie-backend-cswj.onrender.com/api/auth/google/callback",
       passReqToCallback: true,
     },
     async function (req, accessToken, refreshToken, profile, done) {
